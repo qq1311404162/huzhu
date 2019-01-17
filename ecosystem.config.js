86320eb5f9f350cc21a0de8ca4f9d896bc1cbd22
闲庭梦若 <1311404162@qq.com>
2019-01-17 12:14:04 +0800

    g

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
 		instances: 1, // 应用启动实例个数，仅在cluster模式有效 默认为fork；或者 max
 		autorestart: true, // 默认为true, 发生异常的情况下自动重启
 		watch: true, // 是否监听文件变动然后重启
-		ignore_watch: ['node_modules', 'static'], // 不用监听的文件
+		ignore_watch: ['node_modules', 'static', 'logs', 'client/huzhu'], // 不用监听的文件
 		max_memory_restart: '2G', // 最大内存限制数，超出自动重启
 		error_file: './logs/pm2-err.log', // 错误日志文件
 		env: {
